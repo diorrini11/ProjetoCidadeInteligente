bddefd73ae36a36525a2f7da966dc3523ac198dd
diorrini11 <diogoribeirocoelho@gmail.com>
2020-10-25 22:14:57 +0000

    Criação Menu

--- app/src/main/java/com/example/projetocidadeinteligente/MainActivity.kt
+++ app/src/main/java/com/example/projetocidadeinteligente/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.projetocidadeinteligente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean
+    {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        return when (item.itemId)
+        {
+            R.id.optionAdd -> true
+            R.id.optionRemove -> true
+            R.id.optionEdit -> true
+            R.id.optionSelectM -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
