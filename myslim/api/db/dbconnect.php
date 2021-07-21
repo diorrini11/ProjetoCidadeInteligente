13057e9257afd35b5b2436fceb6913c5a3e7251b
Diorrini11 <diogoribeirocoelho@gmail.com>
2021-07-21 02:18:29 +0100

    Add files via upload

--- myslim/api/db/dbconnect.php
+++ myslim/api/db/dbconnect.php
@@ -4,4 +4,12 @@
 	$pass = "";
 	$conn = new PDO('mysql:host=localhost;dbname=cidadeinteligente;charset=utf8', $user, $pass);
 	$db = new NotORM($conn);
+
+	/*
+	include "NotORM.php";
+	$user = "id17282678_dio";
+	$pass = "d~w0GE[c|P>3nXWc";
+	$conn = new PDO('mysql:host=localhost;dbname=id17282678_academycity;charset=utf8', $user, $pass);
+	$db = new NotORM($conn);
+	*/
 ?>
